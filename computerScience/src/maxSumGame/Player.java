2554145d5fb5129662857ceeabdec574a0396e7d
Pinhas <piniziv8@gmail.com>
2021-04-27 11:36:16 +0300

    fixing condition problem in Player.optimalChoice().

--- computerScience/src/maxSumGame/Player.java
+++ computerScience/src/maxSumGame/Player.java
@@ -23,14 +23,14 @@ public abstract class Player {
 	
 	public Direction optimalChoice(int left, int right) {
 
-//		int x = ((left + 2) <= right) ? board.getOptionTable()[left + 2][right] : 0; // x gets max value from left+2 to right.
+		int x = ((left + 2) <= right) ? board.getOptionTable()[left + 2][right] : 0; // x gets max value from left+2 to right.
 		int y = ((left + 1) <= (right - 1)) ? board.getOptionTable()[left + 1][right - 1] : 0; // x gets max value from left+1 to right-1.
 		int z = (left <= (right - 2)) ? board.getOptionTable()[left][right - 2] : 0; // x gets max value from left to right-2.
-
-//		int maxLeft = board.getCards()[left] + Math.min(x, y); // gets the max value if player chose the left card.
+		
+		int maxLeft = board.getCards()[left] + Math.min(x, y); // gets the max value if player chose the left card.
 		int maxRight = board.getCards()[right] + Math.min(y, z); // gets the max value if player chose the right card.
 		
-		if(board.getOptionTable()[left][right] == maxRight) {
+		if(maxRight > maxLeft) {
 			return Direction.RIGHT;
 		} else {
 			return Direction.LEFT;
